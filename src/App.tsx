814899e253d7ed12460fa2f888491f03a19fc75c
kkvzx <64148572+kkvzx@users.noreply.github.com>
2022-06-13 15:40:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,3 +18,9 @@ export const App = () => {
     </div>
   );
 };
+
+// Aplikacja składa się ze strony tytułowej i strony QuizApp gdzie odbywa się praca programu.
+// Quiz app pobiera dane z bazy i na ich podstawie tworzy komponenty (5 bloków pytan + odpowiedzi), odbywa się w niej również sprawdzanie poprawności oraz weryfikacja tego czy odpowiedzi są zaznaczone, a także reset całego programu.
+// SingleQuestion.tsx to pojedynczy blok, który tworzy na podstawie przekazanych mu danych bloki z odpowiedziami (2-4).
+// BtnComponent.tsx to każda z odpowiedzi.
+// Praca programu odbywa się w elemencie QuizApp.tsx na podstawie danych dostarczonych do niego z BtnComponent.tsx (przesyłam do każdego z guzików funkcję edytującą state znajdujące się w QuizApp)
